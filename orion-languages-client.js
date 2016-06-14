bf600fbbbb7e57c3af7f99b220962922e9f54786
Joadr <joaquin.diaz@ies.cl>
2016-06-14 15:43:41 -0400

    fixed for publishing

--- orion-languages-client.js
+++ orion-languages-client.js
@@ -5,5 +5,5 @@ Template.registerHelper('dictlang', function(name, defaultValue) {
 Meteor.startup(function() {
   var userLang = navigator.language || navigator.userLanguage;
   userLang = userLang.split('-')[0];
-  Session.set('lang', 'en');
+  Session.set('lang', userLang);
 });
